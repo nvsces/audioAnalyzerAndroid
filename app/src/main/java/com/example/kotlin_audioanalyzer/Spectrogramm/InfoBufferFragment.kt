7cf9f147cbbb1951fb63a444bfc567c30cecccf3
nvsces <nvsces@yandex.ru>
2020-10-17 14:43:06 +0300

    create menu

--- app/src/main/java/com/example/kotlin_audioanalyzer/Spectrogramm/InfoBufferFragment.kt
+++ app/src/main/java/com/example/kotlin_audioanalyzer/Spectrogramm/InfoBufferFragment.kt
@@ -3,15 +3,14 @@ package com.example.kotlin_audioanalyzer.Spectrogramm
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import com.example.kotlin_audioanalyzer.R
 import com.example.kotlin_audioanalyzer.utils.countVoice
 import com.example.kotlin_audioanalyzer.utils.etalonRun
 import com.example.kotlin_audioanalyzer.utils.realtimebolean
+import com.example.kotlin_audioanalyzer.utils.showToast
 import kotlinx.android.synthetic.main.fragment_info_buffer.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,19 +21,12 @@ import kotlin.coroutines.CoroutineContext
 class InfoBufferFragment(var listBuffer:ArrayList<ShortArray>) : Fragment(R.layout.fragment_info_buffer) {
     private lateinit var nativeLib: SoundEngine
     var fftResolution=1024
-
-
-
     private lateinit var bufferStack // Store trunks of buffers
             : FloatArray
     private lateinit var ton:FloatArray
     private lateinit var tempton:FloatArray
-
     private lateinit var tempReal:FloatArray
-
     private  var bufferRealTime =ArrayList<Float>()
-    private lateinit var fftBuffer // buffer supporting the fft process
-            : ShortArray
     private lateinit var re // buffer holding real part during fft process
             : FloatArray
     private lateinit var im // buffer holding imaginary part during fft process
@@ -45,19 +37,30 @@ class InfoBufferFragment(var listBuffer:ArrayList<ShortArray>) : Fragment(R.layo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
         this.nativeLib = SoundEngine()
         this.nativeLib.initFSin()
         Voicerecorder= VoiceRecord(44100)
         Voicerecorder?.prepare(1024)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.sittings_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.settings_menu_spectr-> showToast("Меню спектр")
+            R.id.settings_menu_graph-> showToast("Меню график")
+        }
+        return true
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-
     }
 
     private fun initBtn() {
-
         info_start.setOnClickListener {
             if (!etalonRun) {
                 etalonRun = true
@@ -72,13 +75,9 @@ class InfoBufferFragment(var listBuffer:ArrayList<ShortArray>) : Fragment(R.layo
     }
 
     private fun getTrunks(recordBuffer: ShortArray) {
-
-
-
-       val  fftReal=fft_(nativeLib,recordBuffer,fftResolution)
+       val  fftReal=fft(nativeLib,recordBuffer,fftResolution)
         val temp=FloatArray(fftResolution)
         System.arraycopy(fftReal, 0, temp, 0, fftResolution/2)
-
         val maxIdx = temp.indices.maxBy { temp[it] } ?: -1
         bufferRealTime.add(maxIdx.toFloat())
        frq_view.setRealTimeWave(bufferRealTime)
@@ -88,26 +87,18 @@ class InfoBufferFragment(var listBuffer:ArrayList<ShortArray>) : Fragment(R.layo
         }
     }
 
-    private fun TimeProcess() {
-
-        frq_view.setWave(listBuffer)
-        frq_view.invalidate()
-
-    }
-
     override fun onResume() {
         super.onResume()
         frq_view.setBackgroundColor(Color.BLACK)
-        FrequencyProcess()
+        frequencyProcess()
         initBtn()
-        //TimeProcess()
     }
 
     fun listShortArray2FloatArray(){
 
     }
 
-    fun fft_(nativeLib: SoundEngine,input: ShortArray,fftResolution:Int): FloatArray {
+    private fun fft(nativeLib: SoundEngine, input: ShortArray, fftResolution:Int): FloatArray {
 
         var real = FloatArray(fftResolution)
         var imag = FloatArray(fftResolution)
@@ -123,7 +114,7 @@ class InfoBufferFragment(var listBuffer:ArrayList<ShortArray>) : Fragment(R.layo
         return real
     }
 
-    private fun FrequencyProcess() {
+    private fun frequencyProcess() {
         val n = fftResolution
 
         re = FloatArray(n)
@@ -132,7 +123,7 @@ class InfoBufferFragment(var listBuffer:ArrayList<ShortArray>) : Fragment(R.layo
         var arrayTon=FloatArray(listBuffer.size)
 
         tempReal= FloatArray(n/2)
-        re=fft_(nativeLib,listBuffer[2],n)
+        re=fft(nativeLib,listBuffer[2],n)
         System.arraycopy(re, 0, tempReal, 0, re.size/2)
 
         val maxIdx = tempReal.indices.maxBy { tempReal[it] } ?: -1
@@ -148,7 +139,7 @@ class InfoBufferFragment(var listBuffer:ArrayList<ShortArray>) : Fragment(R.layo
         bufferStack=tempReal
         for (i in 1 until listBuffer.size){
 
-            re=fft_(nativeLib,listBuffer[i],n)
+            re=fft(nativeLib,listBuffer[i],n)
 
             System.arraycopy(re, 0, tempReal, 0, re.size/2)
 
@@ -159,20 +150,7 @@ class InfoBufferFragment(var listBuffer:ArrayList<ShortArray>) : Fragment(R.layo
             }
             ton=ton+tempton
         }
-       // val m=bufferStack.size
-
- // Move to polar base
-
-        //нормирование
-//        val maXIndex=ton.maxByOrNull { it }!!
-//
-//        for (i in 0 until ton.size){
-//            ton[i]=ton[i]/maXIndex
-//        }
-       // frq_view.setWaveFrequency(bufferStack)
-       // frq_view.setWaveFrequency(ton)
         frq_view.setWaveFrequency(arrayTon)
         frq_view.invalidate()
-         // Clear imaginary part
     }
 }
